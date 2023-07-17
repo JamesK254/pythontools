9a612dec0f683d0f42052a4ae17b698eff425c3a
James <cartelboy254@gmail.com>
2023-07-17 21:39:07 +0300

    updated

--- downloadTorrent.py
+++ downloadTorrent.py
@@ -1,3 +1,3 @@
 from torrentp import TorrentDownloader
-torrent_file = TorrentDownloader("test.torrent", '.')
+torrent_file = TorrentDownloader("payback.torrent", '.')
 torrent_file.start_download()
